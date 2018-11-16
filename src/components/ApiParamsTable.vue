b8208c5ce3f42c5784e272b5ee059ca022291504
weijian <415794089@qq.com>
2018-11-16 16:58:00 +0800

    修复可选参数问题

--- src/components/ApiParamsTable.vue
+++ src/components/ApiParamsTable.vue
@@ -2,14 +2,18 @@
 	<div class="api-params" v-if="data">
 	  <h4>{{ title }}</h4>
 	  <el-table :data="data" border>
-	    <el-table-column prop="key" label="参数名"></el-table-column>
+	    <el-table-column label="参数名">
+	    	<template slot-scope="scope">
+	    		<span v-html="scope.row.key"></span>
+	    		<el-tag size="mini" class="tag-optional-params" v-if="scope.row.disabled && scope.row.disabled == true">可选</el-tag>
+	    	</template>
+	    </el-table-column>
 	    <el-table-column prop="value" label="值" v-if="showValue"></el-table-column>
 	    <el-table-column label="描述">
 	    	<template slot-scope="scope">
 	    		<span v-html="scope.row.description"></span>
 	    	</template>
 	    </el-table-column>
-	    <el-table-column prop="disabled" label="可选参数" width="80"></el-table-column>
 	  </el-table>
 	</div>
 </template>
@@ -50,5 +54,9 @@
 	.api-params h4 {
 		margin: 0;
 		margin-bottom: 10px;
+	}
+
+	.tag-optional-params {
+		margin-left: 4px;
 	}
 </style>
